d2819dce5fa02ee993566a1107720dc5c259d38a
NastyaBondarenko <av_bondarenko@rambler.ru>
2022-05-25 01:57:48 +0300

    add starter

---
+++ src/main/java/com/bondarenko/DefaultFileAnalyzerStarter.java
@@ -0,0 +1,19 @@
+package com.bondarenko;
+
+import com.bondarenko.fileanalyzer.defaultfileanalyzer.DefaultFileAnalyzer;
+import com.bondarenko.fileinformation.FileInformation;
+
+public class DefaultFileAnalyzerStarter {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Make sure you pass two arguments: file path and keyword");
+        }
+        DefaultFileAnalyzer defaultFileAnalyzer = new DefaultFileAnalyzer();
+
+        String path = args[0];
+        String word = args[1];
+
+        FileInformation fileInformation = defaultFileAnalyzer.analyze(path, word);
+        System.out.println(defaultFileAnalyzer.formatResult(fileInformation));
+    }
+}
